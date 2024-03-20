b237900242a874b78c01a84a7240e057052d4caf
Rafael Palamim <palamimweb@gmail.com>
2024-03-20 01:50:53 -0300

    Add initial policy controller

--- src/modules/policy/PolicyController.ts
+++ src/modules/policy/PolicyController.ts
@@ -1,7 +1,19 @@
+import RequestHandler from '../../@core/io/RequestHandler'
 import PolicyService from './PolicyService'
+import { CreateNewDocumentPolicyInputDTO } from './dto/CreateNewDocumentPolicyDTO'
 
 export default class PolicyController {
 
-    constructor(private readonly service: PolicyService) { }
+    constructor(
+        private readonly service: PolicyService
+    ) { }
+
+    async createDocumentPolicy(
+        request: RequestHandler<CreateNewDocumentPolicyInputDTO, null>
+    ): Promise<void> {
+
+        const policy = await this.service.createNewDocumentPolicy(request.body)
+
+    }
 
 }
